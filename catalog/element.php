41feb8b6d1f420eff0f3d186146909115760f7a7
Rasul <rasuldev@gmail.com>
2017-02-28 23:37:37 +0300

    fixed pagenavigation

--- catalog/element.php
+++ catalog/element.php
@@ -1,7 +1,7 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("");?>
 
-
+<? var_dump($_REQUEST['SECTION_CODE']) ?>
 
 
 <?$APPLICATION->IncludeComponent(
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,10 +1,28 @@
 <?
 $arUrlRewrite = array(
 	array(
+		"CONDITION" => "#^/catalog/(.+?)/page/(\\d+)/?(\\?.*)?\$#",
+		"RULE" => "SECTION_CODE=\$1&PAGEN_1=\$2",
+		"ID" => "",
+		"PATH" => "/catalog/section.php",
+	),
+	array(
+		"CONDITION" => "#^/catalog/.+?/(?!\\?)(.+?)/?(\\?.*)?\$#",
+		"RULE" => "ELEMENT_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/catalog/element.php",
+	),
+	array(
 		"CONDITION" => "#^/bitrix/services/ymarket/#",
 		"RULE" => "",
 		"ID" => "",
 		"PATH" => "/bitrix/services/ymarket/index.php",
+	),
+	array(
+		"CONDITION" => "#^/catalog/(.+?)/?(\\?.*)?\$#",
+		"RULE" => "SECTION_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/catalog/section.php",
 	),
 	array(
 		"CONDITION" => "#^/personal/order/#",
@@ -24,18 +42,6 @@ $arUrlRewrite = array(
 		"ID" => "bitrix:news",
 		"PATH" => "/news/index.php",
 	),
-    array(
-        "CONDITION" => "#^/catalog/.+?/(.+?)(/.*)?$#",
-        "RULE" => "ELEMENT_CODE=$1",
-        "ID" => "",
-        "PATH" => "/catalog/element.php",
-    ),
-    array(
-        "CONDITION" => "#^/catalog/(.+?)(/.*)?$#",
-        "RULE" => "SECTION_CODE=$1",
-        "ID" => "",
-        "PATH" => "/catalog/section.php",
-    )
 );
 
 ?>
