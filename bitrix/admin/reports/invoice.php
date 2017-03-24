7a6fd2d5ffaa2085b7d0eabdd39edd6d283c39bf
ismag <ismag@example.com>
2017-03-24 22:05:49 +0300

    order form

---
+++ bitrix/admin/reports/invoice.php
@@ -0,0 +1,4 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?><?
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/admin/reports/ru/reports/invoice.php"))
+	include($_SERVER["DOCUMENT_ROOT"]."/bitrix/admin/reports/ru/reports/invoice.php");
+?>
